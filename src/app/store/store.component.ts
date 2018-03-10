0c8ec1d8947bb5905f66a4ae284f1628dee771e2
Victor Sc <victor.sc175@gmail.com>
2018-03-10 11:18:49 +0200

    Add cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
 
 @Component({
   selector: "store",
@@ -13,7 +14,8 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 	
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository,
+  	      private cart: Cart) { }
   
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1 ) * this.productsPerPage
@@ -42,5 +44,9 @@ export class StoreComponent {
   	return Array(Math.ceil(this.repository
   		.getProducts(this.selectedCategory).length / this.productsPerPage))
   		.fill(0).map((x, i) => i + 1);
+  }
+  
+  addProductToCart(product: Product) {
+  	this.cart.addLine(product);
   }
 }
