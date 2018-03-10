2765c246a08bda0b978772359bd400dd1f870e3e
Victor Sc <victor.sc175@gmail.com>
2018-03-10 17:11:03 +0200

    Add cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: "<store></store>"
+  template: "<router-outlet></router-outlet>"
 })
 export class AppComponent {
   title = 'app';
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
-import { CartSummaryComponent } from "./cartSummary.component"
+import { CartSummaryComponent } from "./cartSummary.component";
+import { CartDetailComponent } from "./cartDetail.component";
+import { CheckoutComponent } from "./checkout.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CartSummaryComponent],
-  exports: [StoreComponent]	
+  imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+  declarations: [StoreComponent,
+                 CartSummaryComponent,
+                 CartDetailComponent,
+                 CheckoutComponent],
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent]	
 })
 
 export class StoreModule { }
