d63341015507b506580f6c22b72a541f0c6a60ee
Victor Sc <victor.sc175@gmail.com>
2018-03-10 17:29:47 +0200

    Add checkout

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
-import { Order } from "./order/model";
+import { Order } from "./order.model";
 import { StaticDataSource } from "./static.datasource";
 
 @Injectable()
